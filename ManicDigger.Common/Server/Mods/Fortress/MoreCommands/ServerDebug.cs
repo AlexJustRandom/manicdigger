4026a2c758c99e6733f9d17c69520c39eb9473a6
AlexJustRandom <incognitodeveloper@tutanota.com>
2023-12-16 01:48:22 +0100

    Update ServerDebug.cs

--- ManicDigger.Common/Server/Mods/Fortress/MoreCommands/ServerDebug.cs
+++ ManicDigger.Common/Server/Mods/Fortress/MoreCommands/ServerDebug.cs
@@ -10,63 +10,58 @@ using System;
 namespace ManicDigger.Mods
 {
     public class ServerDebug : IMod
-	{
+    {
         ModManager m;
         public void PreStart(ModManager m)
-		{
-			m.RequireMod("CoreBlocks");
-		}
+        {
+            m.RequireMod("CoreBlocks");
+        }
 
-		public void Start(ModManager manager)
-		{
-			m = manager;
-			iRestartInterval = m.GetAutoRestartInterval();
+        public void Start(ModManager manager)
+        {
+            m = manager;
 
-			m.RegisterOnCommand(TeleportToID);
-			m.RegisterCommandHelp("SetModSpeed", "Teleports you to the player with the given ID");
-			
-			m.RegisterTimer(Uptime_Tick, (double)1);
-			
+            m.RegisterOnCommand(TeleportToID);
+            m.RegisterCommandHelp("SetModSpeed", "Teleports you to the player with the given ID");
 
-		}
-	
-		
 
-			
-		 
-		bool TeleportToID(int player, string command, string argument)
-		{
-			if (command.Equals("SetModSpeed", StringComparison.InvariantCultureIgnoreCase))
-			{
-			//	if (!m.PlayerHasPrivilege(player, "tp"))
-				//{
-				//	m.SendMessage(player, chatPrefix + m.colorError() + GetLocalizedString(0));
-				//	System.Console.WriteLine(string.Format("[ServerUtilities] {0} tried to teleport by ID (no permission)", m.GetPlayerName(player)));
-				//	return true;
-				//}
-				int targetID;
-				try
-				{
-					targetID = Convert.ToInt32(argument);
-				}
-				catch (FormatException)
-				{
-					m.SendMessage(player, chatPrefix + m.colorError() + GetLocalizedString(1));
-					return true;
-				}
-				catch (OverflowException)
-				{
-					m.SendMessage(player, chatPrefix + m.colorError() + GetLocalizedString(2));
-					return true;
-				}
-                m.VIPDEBUGTEST("spmod", val);
- 				
-			}
-			return false;
-		}
+        }
 
-	 
-	 
-		
-	}
+
+
+
+
+        bool TeleportToID(int player, string command, string argument)
+        {
+            if (command.Equals("SetModSpeed", StringComparison.InvariantCultureIgnoreCase))
+            {
+                //  if (!m.PlayerHasPrivilege(player, "tp"))
+                //{
+                //  m.SendMessage(player, chatPrefix + m.colorError() + GetLocalizedString(0));
+                //  System.Console.WriteLine(string.Format("[ServerUtilities] {0} tried to teleport by ID (no permission)", m.GetPlayerName(player)));
+                //  return true;
+                //}
+                int targetID;
+                try
+                {
+                    targetID = Convert.ToInt32(argument);
+                }
+                catch (FormatException)
+                {
+                    return true;
+                }
+                catch (OverflowException)
+                {
+                    return true;
+                }
+                m.VIPDEBUGTEST("spmod", targetID);
+
+            }
+            return false;
+        }
+
+
+
+
+    }
 }
